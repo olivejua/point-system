188d5b4d45066a5ec67ae22246073822bb886d1f
olivejua <tmfrl4710@gmail.com>
2022-05-01 19:57:48 +0900

    Initial commit

---
+++ src/test/java/dev/olivejua/pointsystem/PointSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olivejua.pointsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
