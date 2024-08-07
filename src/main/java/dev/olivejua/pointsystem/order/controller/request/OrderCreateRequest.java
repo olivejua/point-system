8d23a0543307390603cd7dfbf0a6e626cf3eca68
seulki <tmfrl4710@gmail.com>
2024-08-07 21:52:20 +0900

    refactor: OrderCreateRequest 유효성 애노테이션 추가

--- src/main/java/dev/olivejua/pointsystem/order/controller/request/OrderCreateRequest.java
+++ src/main/java/dev/olivejua/pointsystem/order/controller/request/OrderCreateRequest.java
@@ -1,7 +1,13 @@
 package dev.olivejua.pointsystem.order.controller.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+
 public record OrderCreateRequest(
+        @NotNull @PositiveOrZero
         long productId,
+        @NotNull @PositiveOrZero
         long customerId,
+        @PositiveOrZero
         long points
 ) {}
